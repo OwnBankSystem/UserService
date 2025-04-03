bcb71426ff216c79173a779015968ba05c92db34
Hanif0610 <marbling1293@naver.com>
2025-04-04 01:28:11 +0900

    [ADD] Project Init

---
+++ src/test/kotlin/com/BankSystem/UserService/UserServiceApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.BankSystem.UserService
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class UserServiceApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
