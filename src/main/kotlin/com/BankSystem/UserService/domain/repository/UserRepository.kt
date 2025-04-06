61a6d6e9b6af1b436b8714281f4b2d20b7e8170e
Hanif0610 <marbling1293@naver.com>
2025-04-06 21:53:48 +0900

    [ADD] test h2 의존성 추가

--- src/main/kotlin/com/BankSystem/UserService/domain/repository/UserRepository.kt
+++ src/main/kotlin/com/BankSystem/UserService/domain/repository/UserRepository.kt
@@ -2,7 +2,9 @@ package com.BankSystem.UserService.domain.repository
 
 import com.BankSystem.UserService.domain.entity.User
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.stereotype.Repository
 
+@Repository
 interface UserRepository : JpaRepository<User, Long> {
 
 }
