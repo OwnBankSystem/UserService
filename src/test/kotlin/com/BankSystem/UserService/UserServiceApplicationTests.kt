38925bc375de1ee09cc0ac51aeeac124508e9189
Hanif0610 <marbling1293@naver.com>
2025-04-08 01:34:49 +0900

    [DELETE] 불필요 어노테이션 제거

--- src/test/kotlin/com/BankSystem/UserService/UserServiceApplicationTests.kt
+++ src/test/kotlin/com/BankSystem/UserService/UserServiceApplicationTests.kt
@@ -2,9 +2,7 @@ package com.BankSystem.UserService
 
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
-import org.springframework.test.context.ActiveProfiles
 
-@ActiveProfiles("test")
 @SpringBootTest(classes = [UserServiceApplication::class])
 class UserServiceApplicationTests {
 
